fe6f9d2c0b7621330098ccd5558d8c1e86b21c55
jayanth karnati <164364265+Jayanth421@users.noreply.github.com>
2026-07-28 11:00:17 +0530

    Update UserInfoCard.tsx

--- src/components/UserProfile/UserInfoCard.tsx
+++ src/components/UserProfile/UserInfoCard.tsx
@@ -16,7 +16,7 @@ export default function UserInfoCard() {
       <div className="flex flex-col gap-6 lg:flex-row lg:items-start lg:justify-between">
         <div>
           <h4 className="text-lg font-semibold text-gray-800 dark:text-white/90 lg:mb-6">
-            Personal Information
+           student Information
           </h4>
 
           <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-7 2xl:gap-x-32">
@@ -25,7 +25,7 @@ export default function UserInfoCard() {
                 First Name
               </p>
               <p className="text-sm font-medium text-gray-800 dark:text-white/90">
-                Musharof
+                rakesh
               </p>
             </div>
 
@@ -43,25 +43,33 @@ export default function UserInfoCard() {
                 Email address
               </p>
               <p className="text-sm font-medium text-gray-800 dark:text-white/90">
-                randomuser@pimjo.com
+                25h51a0485@cmrcet.ac.i
               </p>
             </div>
 
+            <div>
+              <p className="mb-2 text-xs leading-normal text-gray-500 dark:text-gray-400">
+                Roll number
+              </p>
+              <p className="text-sm font-medium text-gray-800 dark:text-white/90">
+               25H51A0485
+              </p>
+            </div>
             <div>
               <p className="mb-2 text-xs leading-normal text-gray-500 dark:text-gray-400">
                 Phone
               </p>
               <p className="text-sm font-medium text-gray-800 dark:text-white/90">
-                +09 363 398 46
+                +91 8341852890
               </p>
             </div>
 
             <div>
               <p className="mb-2 text-xs leading-normal text-gray-500 dark:text-gray-400">
-                Bio
+                class
               </p>
               <p className="text-sm font-medium text-gray-800 dark:text-white/90">
-                Team Manager
+                2year-ECE-B
               </p>
             </div>
           </div>
